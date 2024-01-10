de38fb687f1948f1b2288f16704730054c464773
Leuenberger <nimai.leuenberger@zuhlke.com>
2024-01-10 14:44:19 +0100

    initial setup number 2

---
+++ app/src/main/java/com/runtracker/activity/MainActivity.java
@@ -0,0 +1,17 @@
+package com.runtracker.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.runtracker.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
---
+++ app/src/main/java/com/runtracker/activity/RunningOverviewActivity.java
@@ -0,0 +1,16 @@
+package com.runtracker.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.runtracker.R;
+
+public class RunningOverviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_running_overview);
+    }
+}
